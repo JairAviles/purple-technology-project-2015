4c174954c0eaf07734158a133f6915ec4cd31881
Jair Avilés <hi@jairaviles.mx>
2015-08-29 15:47:15 -0500

    [Update] package.json, added engines for Heroku deployment
    [Add] Procfile file

--- exchange-rates/config/config.js
+++ exchange-rates/config/config.js
@@ -4,8 +4,8 @@ var config = {};
 // Object Definition
 config.currencies_list = {};
 
-config.node_env = process.env.NODE_ENV || "production"; //Enviroment
-config.api_open_exchange_rates_key = process.env.API_OPEN_EXCHANGE_RATES_KEY || "0bde6c73f7754a0da05461c28e769000"; //API Id
+config.node_env = process.env.NODE_ENV || "development"; //Enviroment
+config.api_open_exchange_rates_key = process.env.API_OPEN_EXCHANGE_RATES_KEY; //API Id
 config.version = process.env.VERSION || "RC1"; //Current api version
 config.baseCurrency = "USD"; //Base Currency
 config.currencies_list = {
